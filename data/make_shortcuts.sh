44b21de397115c07a9491e7909c96bc99a24212e
foragingBRAIN <zjumao@gmail.com>
2019-05-09 17:59:16 -0500

    a

---
+++ data/make_shortcuts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This bash script place training and commit shortcuts on Desktop
+
+# training shortcut
+cp ~/Documents/git/rPi/foraging ~/Desktop/foraging
+
+# synch shortcut
+cp ~/Documents/git/rPimemory/synchmemory ~/Desktop/synchmemory
